62fdfd9f997c99e4c91c5af2f28c251253e258dd
Xuwei Li <dev@xuwei.li>
2022-02-07 05:10:12 -0800

    Add Prev and Next navigation.

--- src/routes/github/[id].svelte
+++ src/routes/github/[id].svelte
@@ -1,15 +1,55 @@
-<script>
+<script lang='ts'>
+    import {
+        goto,
+        invalidate
+    } from '$app/navigation';
+
     export let id;
     export let joke;
+
+    let idValue = parseInt(id);
+    $: prevIdValue = idValue - 1;
+    $: nextIdValue = idValue + 1;
+    $: imageSrc = `https://avatars.githubusercontent.com/u/${idValue}`
+    $: title = `Github User ${idValue} Avatar`
+
+    let handlePrev = (e) => {
+        e.preventDefault();
+        invalidate(`/github/${idValue}`);
+        idValue--;
+        goto(`/github/${idValue}`, {replaceState: true});
+    }
+    let handleNext = (e) => {
+        e.preventDefault();
+        invalidate(`/github/${idValue}`);
+        idValue++;
+        goto(`/github/${idValue}`, {replaceState: true});
+    }
 </script>
 
 <svelte:head>
-	<title>Github User {id} Avatar</title>
+	<title>{title}</title>
 </svelte:head>
 
 <div>
     <figure>
-        <img src="https://avatars.githubusercontent.com/u/{id}" alt="">
+        <img src={imageSrc} alt="">
         <figcaption>{joke}</figcaption>
     </figure>
 </div>
+
+<div class="nav">
+    {#if idValue > 1}
+        <a class="prev" href="/github/{prevIdValue}" on:click={handlePrev}>Prev</a>
+    {/if}
+    <a class="next" href="/github/{nextIdValue}" on:click={handleNext}>Next</a>
+</div>
+
+<style>
+.nav {
+    margin-left: 3rem;
+}
+.next {
+    margin-left: 2rem;
+}
+</style>
