4513b5a1904ba0f9c62c9bf1384594d8aa1f61d8
Xuwei Li <dev@xuwei.li>
2022-02-07 01:52:18 -0800

    Debug /api.

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,10 +1,12 @@
 export async function get({ request, platform }) {
 	// const KV = platform.env.KV;
+    const KV2 = platform.env.KV2;
 
     return {
         body: {
             env: Object.entries(platform.env),
-            kv_svelte: await platform.env.KV.get('svelte')
+            kv_svelte: await platform.env.KV.get('svelte'),
+            kv2_svelte: await KV2.get('svelte'),
         }
     };
 }
